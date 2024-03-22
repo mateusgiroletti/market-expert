993ff0e99f28d9104a2c69223cabbca6a4fff982
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-03-21 21:21:58 -0300

    config: db script

--- backend/.docker/postgres/initdb.sql
+++ backend/.docker/postgres/initdb.sql
@@ -1,6 +1,46 @@
 DO $$ 
 BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'market_express') THEN
-        CREATE DATABASE market_express;
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'market_expert') THEN
+        CREATE DATABASE market_expert;
     END IF;
+
+    CREATE TABLE IF NOT EXISTS product_type (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS product_type_taxes (
+        id SERIAL PRIMARY KEY,
+        product_type_id INT NOT NULL,
+        percentual INT NOT NULL,
+        FOREIGN KEY (product_type_id) REFERENCES product_type(id)
+    );
+
+
+    CREATE TABLE IF NOT EXISTS product (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        product_type_id INT NULL,
+        price NUMERIC(10, 2) NOT NULL,
+        FOREIGN KEY (product_type_id) REFERENCES product_type(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS sales (
+        id SERIAL PRIMARY KEY,
+        total_purchase NUMERIC(10, 2) NOT null,
+        total_tax NUMERIC(10, 2) NOT null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE IF NOT EXISTS sales_products (
+        id SERIAL PRIMARY KEY,
+        sale_id INT NOT NULL,
+        product_type_id INT NOT NULL,
+        amount INT NOT NULL,
+        subtotal NUMERIC(10, 2) NOT null,
+        total_tax NUMERIC(10, 2) NOT null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (sale_id) REFERENCES sales(id),
+        FOREIGN KEY (product_type_id) REFERENCES product_type(id)
+    );
 END $$;
