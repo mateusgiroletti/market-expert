9334eaeb54677b780275a4e4e167fdeeaddf03e0
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-03-25 17:51:56 -0300

    fix: validate

--- backend/src/Infra/Utils/Validator.php
+++ backend/src/Infra/Utils/Validator.php
@@ -2,12 +2,15 @@
 
 namespace Infra\Utils;
 
+use ErrorException;
+
 class Validator
 {
     public static function validateNotEmpty(array $data, array $fields): void
     {
         foreach ($fields as $field) {
             if (!isset($data[$field]) || empty($data[$field])) {
+                throw new ErrorException("{$field} is required");
                 http_response_code(400);
                 $response = ['error' => "{$field} is required"];
                 echo json_encode($response);
@@ -19,6 +22,7 @@ class Validator
     {
         foreach ($fields as $field) {
             if (!isset($data[$field]) || !is_numeric($data[$field]) || $data[$field] <= 0) {
+                throw new ErrorException("{$field} must be a positive number");
                 http_response_code(400);
                 $response = ['error' => "{$field} must be a positive number"];
                 echo json_encode($response);
@@ -30,6 +34,7 @@ class Validator
     {
         foreach ($fields as $field) {
             if (isset($data[$field]) && strlen($data[$field]) > $maxLength) {
+                throw new ErrorException("{$field} must be at most {$maxLength} characters long");
                 http_response_code(400);
                 $response = ['error' => "{$field} must be at most {$maxLength} characters long"];
                 echo json_encode($response);
@@ -41,6 +46,7 @@ class Validator
     {
         foreach ($fields as $field) {
             if (isset($data[$field]) && strlen($data[$field]) < $minLength) {
+                throw new ErrorException("{$field} must be at least {$minLength} characters");
                 http_response_code(400);
                 $response = ['error' => "{$field} must be at least {$minLength} characters"];
                 echo json_encode($response);
