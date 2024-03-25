291c07e1ca2e5331fe4a9c2f29e519ae7d53be74
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-03-25 18:50:43 -0300

    feat: refactoring home component

--- frontend/src/view/pages/Home/index.tsx
+++ frontend/src/view/pages/Home/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
                     to="/new-product"
                     className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
                 >
-                    Adicionar Novo Produto
+                    Novo Produto
                 </Link>
                 <Link
                     to="/new-sale"
