11af379618b57efd0937cb4c4c1e94ec603f3a3d
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-03-26 19:39:35 -0300

    config: vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        coverage: {
+            enabled: true,
+            reporter: ['text', 'json', 'html'],
+        },
+    }
 })
