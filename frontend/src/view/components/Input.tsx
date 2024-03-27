b618167d5521c987de431d10886224cb87c7c79f
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-03-27 10:13:47 -0300

    feat: input component

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,27 +1,16 @@
 import { ComponentProps } from "react";
 import { cn } from "../../helpers/cn";
 
-interface InputProps extends ComponentProps<"input"> {
-    variant?: "blue" | "green"| "red";
-}
-
 export function Input({
     className,
-    variant,
     ...props
-}: InputProps) {
+}: ComponentProps<"input">) {
     return (
         <input
             data-testid="input"
             {...props}
             className={cn(
                 "w-full px-3 py-2 border rounded shadow-sm",
-                variant === "blue" &&
-                "bg-blue-500 hover:bg-blue-600 focus:bg-blue-600",
-                variant === "green" &&
-                "bg-green-500 hover:bg-green-600 focus:bg-green-600",
-                variant === "red" &&
-                "bg-red-500 hover:bg-red-600 focus:bg-red-600",
                 className,
             )}
         >
